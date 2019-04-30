b5923397a6c2e7e7ad235c0272d4141b065cb4da
Pietro Zuntini Bonfim <pietrozuntini@gmail.com>
2019-04-30 20:17:49 -0300

    Criação dos selects do Estado e Escola
    Co-authored-by: Bianca Gomes <bgsrd@outlook.com>

--- Fase 01 - Web (Front End)/js/escolas.js
+++ Fase 01 - Web (Front End)/js/escolas.js
@@ -1,6 +1,15 @@
 $(document).ready(function() {
 
     // Criação dos selects
-    $("#select-estado").select2();
+    $("#select-estado").select2({
+        placeholder: "Estado"
+    });
+    $("#select-municipio").select2({
+        placeholder: "Município"
+    });
+    $("#select-escola").select2({
+        placeholder: "Escola"
+    });
+    
 
 });
