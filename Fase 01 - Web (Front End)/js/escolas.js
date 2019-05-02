0e7145ae0eb7d5ed0d381c1e5cf835b32738e4ba
Bianca Gomes Rodrigues <bgsrd@outlook.com>
2019-05-02 10:05:17 -0300

    Criação da datatable e CSS
    Co-authored-by: Pietro Zuntini <pietrozuntini@gmail.com>

--- Fase 01 - Web (Front End)/js/escolas.js
+++ Fase 01 - Web (Front End)/js/escolas.js
@@ -1,3 +1,32 @@
+
+// =======================================================
+// Função para iniciar/reinicializar a DataTable
+// =======================================================
+
+function inicializaDataTable() {
+    $('#tabela-escolas').DataTable({
+        "language": {
+            "lengthMenu": "Mostrar _MENU_ itens por página",
+            "zeroRecords": "Nenhum item encontrado - desculpa",
+            "info": "Mostrando página _PAGE_ de _PAGES_",
+            "infoEmpty": "Nenhum item encontrado",
+            "infoFiltered": "(filtrado a partir de _MAX_ itens)",
+            "search": "Buscar:",
+            "emptyTable":     "Nenhum dado disponível na tabela",
+            "loadingRecords": "Carregando...",
+            "processing":     "Processando...",
+            "paginate": {
+                "first":      "Primeiro",
+                "last":       "Último",
+                "next":       "Próximo",
+                "previous":   "Anterior"
+            }
+        },
+        "order": [[ 1, "asc" ]]    // Ordena por Nome
+    });
+    $('.dataTables_length').addClass('bs-select');
+}
+
 $(document).ready(function() {
 
     // Criação dos selects
@@ -10,6 +39,8 @@ $(document).ready(function() {
     $("#select-escola").select2({
         placeholder: "Escola"
     });
-    
+
+
+    inicializaDataTable(); 
 
 });
