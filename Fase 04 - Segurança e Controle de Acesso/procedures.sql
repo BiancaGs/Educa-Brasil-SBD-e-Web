eb8426383a233fab19d7f8247ba8987b03768594
Bianca Gomes Rodrigues <bgsrd@outlook.com>
2019-06-11 20:13:40 -0300

    Procedure da consulta 1
    Co-authored-by: Pietro Zuntini <pietrozuntini@gmail.com>

--- Fase 04 - Segurança e Controle de Acesso/procedures.sql
+++ Fase 04 - Segurança e Controle de Acesso/procedures.sql
@@ -2,15 +2,62 @@
 -- CONSULTA 1
 -- =======================================================
 
--- TODO
-
-CREATE OR REPLACE FUNCTION recuperarEscolas( ....., limit int, offset int )  
-RETURNS TABLE ( COMO RETORNAR TODAS AS COLUNAS DA ESCOLA ) AS $$
+CREATE OR REPLACE FUNCTION recuperarEscolas( campoBusca varchar(100), codigoEstado varchar(2), limite int, deslocamento int, ordColuna int, ordDirecao varchar(4) )  
+RETURNS TABLE (
+    co_escola escola.co_escola%TYPE, nome_escola escola.nome_escola%TYPE,
+    situacao_funcionamento escola.situacao_funcionamento%TYPE,
+    dependencia_adm escola.dependencia_adm%TYPE, bercario escola.bercario%TYPE,
+    creche escola.creche%TYPE, pre_escola escola.pre_escola%TYPE,
+    ens_fundamental_anos_iniciais escola.ens_fundamental_anos_iniciais%TYPE,
+    ens_fundamental_anos_finais escola.ens_fundamental_anos_finais%TYPE,
+    ens_medio_normal escola.ens_medio_normal%TYPE,
+    ens_medio_integrado escola.ens_medio_integrado%TYPE
+) AS $$
+DECLARE
+    sql text := 'SELECT
+        e.co_escola, e.nome_escola, e.situacao_funcionamento, e.dependencia_adm, e.bercario, e.creche, e.pre_escola,
+        e.ens_fundamental_anos_iniciais, e.ens_fundamental_anos_finais, e.ens_medio_normal, e.ens_medio_integrado
+    FROM escola e ';
 BEGIN
-    
+
+    sql := sql || 'WHERE true ';
+
+    IF codigoEstado IS NOT NULL THEN
+        -- Filtra apenas os distritos do estado especificado 
+        sql := sql || 'AND co_distrito IN (
+            SELECT co_distrito
+            FROM distritos' || codigoEstado || ' d)';
+    END IF;
+
+    -- Campo de Busca
+    sql := sql || '
+        AND e.nome_escola LIKE ' || quote_literal('%' || campoBusca || '%') || '
+        ORDER BY ';
+
+    -- Ordenação
+    CASE ordColuna
+		WHEN 0 THEN
+			sql := sql || 'e.co_escola ';
+        WHEN 1 THEN
+			sql := sql || 'e.nome_escola ';
+        WHEN 2 THEN
+			sql := sql || 'e.situacao_funcionamento ';
+        WHEN 3 THEN
+        	sql := sql || 'e.dependencia_adm ';
+	END CASE;
+
+    -- Limite e Deslocamento
+    sql := sql || ordDirecao || '
+        LIMIT ' || limite || ' OFFSET ' || deslocamento;
+
+    -- Retorna a Query
+    RETURN QUERY EXECUTE sql;
 END
 $$ LANGUAGE plpgsql;
 
+
+-- Exemplo de chamada
+SELECT * FROM recuperarEscolas('UNIDADE', '35', 10, 100, 1, 'asc');
 
 
 -- =======================================================
@@ -18,13 +65,15 @@ $$ LANGUAGE plpgsql;
 -- =======================================================
 
 CREATE OR REPLACE FUNCTION recuperarEstatisticas( brasil boolean, codigoRegiao varchar(1), codigoEstado varchar(2), codigoMunicipio varchar(7) )
-RETURNS TABLE ( g_bercario integer, g_creche integer, g_pe integer, g_efi integer,
-                g_efii integer, g_emn integer, g_emi integer, g_situacao integer, g_dep integer,
-                g_localizacao integer, bercario escola.bercario%TYPE, creche escola.creche%TYPE,
-                pre_escola escola.pre_escola%TYPE, ens_fundamental_anos_iniciais escola.ens_fundamental_anos_iniciais%TYPE,
-                ens_fundamental_anos_finais escola.ens_fundamental_anos_finais%TYPE, ens_medio_normal escola.ens_medio_normal%TYPE,
-                ens_medio_integrado escola.ens_medio_integrado%TYPE, situacao_funcionamento escola.situacao_funcionamento%TYPE,
-                dependencia_adm escola.dependencia_adm%TYPE,  localizacao escola.localizacao%TYPE, qtd_escolas bigint ) AS $$
+RETURNS TABLE ( 
+    g_bercario integer, g_creche integer, g_pe integer, g_efi integer,
+    g_efii integer, g_emn integer, g_emi integer, g_situacao integer, g_dep integer,
+    g_localizacao integer, bercario escola.bercario%TYPE, creche escola.creche%TYPE,
+    pre_escola escola.pre_escola%TYPE, ens_fundamental_anos_iniciais escola.ens_fundamental_anos_iniciais%TYPE,
+    ens_fundamental_anos_finais escola.ens_fundamental_anos_finais%TYPE, ens_medio_normal escola.ens_medio_normal%TYPE,
+    ens_medio_integrado escola.ens_medio_integrado%TYPE, situacao_funcionamento escola.situacao_funcionamento%TYPE,
+    dependencia_adm escola.dependencia_adm%TYPE,  localizacao escola.localizacao%TYPE, qtd_escolas bigint 
+) AS $$
 DECLARE
     sql text := 'SELECT
         GROUPING(e.bercario) g_bercario, GROUPING(e.creche) g_creche, GROUPING(e.pre_escola) g_pe, GROUPING(e.ens_fundamental_anos_iniciais) g_efi,
